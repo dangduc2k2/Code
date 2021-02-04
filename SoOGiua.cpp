74f97b09b5530094b22629c0c2f9a0554c441d1a
dangduc2k2 <78548101+dangduc2k2@users.noreply.github.com>
2021-02-04 22:31:36 +0700

    Add files via upload

---
+++ SoOGiua.cpp
@@ -0,0 +1,15 @@
+//Cau 25
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a[5];
+    for (int i=0; i<5; i++) cin >> a[i];
+    sort (a,a+5);
+    cout << a[2];
+	return 0;
+}
