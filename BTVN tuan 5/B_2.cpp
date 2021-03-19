5e053183465d7caed32cc95def0d35dfab79d4e5
Dang Minh Duc <78548101+dangduc2k2@users.noreply.github.com>
2021-03-19 20:27:50 +0700

    Update B_2.cpp

--- BTVN tuan 5/B_2.cpp
+++ BTVN tuan 5/B_2.cpp
@@ -1,7 +1,3 @@
-#include<iostream>
-
-using namespace std;
-
 bool eq(int a[],int b[], int n)
 {
     for (int i=0; i<n; i++)
@@ -11,23 +7,3 @@ bool eq(int a[],int b[], int n)
     return true;
 }
 
-int main()
-{
-    int n;
-    cin >> n;
-    int a[100], b[100];
-    for (int i=0; i<n; i++) cin >> a[i];
-    for (int i=0; i<n; i++) cin >> b[i];
-    for (int i=0; i<n; i++)
-    {
-        if (a[i]!=b[i]) {
-            cout << "false";
-            break;
-        }
-        else {
-            if (i==n-1) cout << "true";
-            continue;
-        }
-    }
-    return 0;
-}
